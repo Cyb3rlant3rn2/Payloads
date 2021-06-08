87a6c88c6e72bbe076e4b20cb02ae2331f524d01
Cyb3rlant3rn2 <64853191+Cyb3rlant3rn2@users.noreply.github.com>
2021-06-08 12:00:55 +0000

    Add files via upload

---
+++ xpath/tmp/run.cmd
@@ -0,0 +1,2 @@
+del "C:\Users\Dell\Desktop\payloads\xpath\tmp\result.htm"
+"C:\Users\Dell\Desktop\payloads\xpath\curl.exe" --config "C:\Users\Dell\Desktop\payloads\xpath\tmp\curl.tmp_config" -k >"C:\Users\Dell\Desktop\payloads\xpath\tmp\result.htm"
